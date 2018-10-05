102370632871270f6955ca9978438cc83ab2b04c
Matthew R Hanlon <matthew.hanlon@oracle.com>
2018-10-05 16:10:13 -0600

    add tmux

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # 1. Ensure modules
+# tmux
+mkdir -p $HOME/.tmux/plugins
+
 
 # 2. Create symlinks
 ln -s $HOME/dotfiles/oh-my-zsh $HOME/.oh-my-zsh
@@ -9,7 +12,8 @@ ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/vim $HOME/.vim
 ln -s $HOME/dotfiles/gitignore_global $HOME/.gitignore_global
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
-
+ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/tmux.conf.local $HOME/.tmux.conf.local
 # 3. ???
 
 # 4. Profit
